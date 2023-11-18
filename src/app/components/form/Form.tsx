6091cc399577302b35833138af21976b4744dbf9
Volodymyr Dziubak <Volodymyr.Dzyubak@gmail.com>
2023-11-18 06:38:29 -0500

    a simple form

---
+++ src/app/components/form/Form.tsx
@@ -0,0 +1,21 @@
+interface Props {
+  action: () => void;
+}
+
+export default function Form() {
+  return (
+    <form className="flex flex-col gap-4">
+      <label className="flex flex-col">
+        Your name:
+        <input type="text" name="name" className="text-slate-900" />
+      </label>
+
+      <label className="flex flex-col">
+        Your email:
+        <input type="email" name="email" className="text-slate-900" />
+      </label>
+
+      <button className="bg-slate-600">Submit</button>
+    </form>
+  );
+}
